668fc93593cacb50b163c925f7bfdc2dc5f4de7d
NoemiRozpara <nrozpara@gmail.com>
2019-01-03 12:32:45 +0100

    check user

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,9 @@ const Nav = (props) => {
     console.log(props.currentCat)
     return (
         <nav>
-            <span onClick={() => props.changeCurrentCat('new')}>New</span>
-            <span onClick={() => props.changeCurrentCat('top')}>Top</span>
-            <span onClick={() => props.changeCurrentCat('best')}>Best</span>
+            <span onClick={() => props.changeCurrentCat('new')}>New </span>
+            <span onClick={() => props.changeCurrentCat('top')}>Top </span>
+            <span onClick={() => props.changeCurrentCat('best')}>Best </span>
         </nav>
     )
 }
